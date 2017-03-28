60b1b85f955c5cadc7f8d405ec4fe274c432ce47
Bailey van Antwerpen <99041848@mydavinci.nl>
2017-03-28 14:07:31 +0200

    update

---
+++ delete.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Verjaardagskalender</title>
+	<link href="main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>je bent geDELETETETETETE</h1>
+</body>
+</html>
